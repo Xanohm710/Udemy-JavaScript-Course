fcc93c3cf5df611987aae93e0d6f9afb7a5e2a37
Jared Young <104388797+Xanohm710@users.noreply.github.com>
2022-12-20 11:07:00 -0600

    Truthy and Falsy Values

--- script.js
+++ script.js
@@ -223,3 +223,25 @@ n = n -1; // 11-1=10
 console.log(n);
 */
 
+// 5 falsy values: 0, '' (empty string), undefined, null, NaN
+
+console.log(Boolean(0)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean('Jonas')); // true
+console.log(Boolean({})); // true
+console.log(Boolean('')); // false
+
+const money = 0;
+if(money) {
+    console.log("Don't spend it all :)");
+}else {
+    console.log('You should get a job!');
+}
+
+let height = 0;
+if(height) {
+    console.log('YAY! Height is defined');
+}else{
+    console.log('Height is UNDEFINED');
+}
+
