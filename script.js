cdddba57ed32e4bb6f8ba466f17b172db9362aba
Jared Young <104388797+Xanohm710@users.noreply.github.com>
2022-12-20 07:19:02 -0600

    if/else Statements

--- script.js
+++ script.js
@@ -132,7 +132,6 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
-*/
 
 const firstName = 'Jared';
 const job = 'developer';
@@ -155,4 +154,26 @@ console.log(`String
 multiple
 lines`);
 
+// If/Else Statements 
+const age = 15;
+
+if(age >= 18) {
+  console.log('Sarah can start driving license 🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another 
+    ${yearsLeft} years :)`);
+}
+
+const birthYear = 1992;
+
+let century;
+if(birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+
+console.log(century);
+*/
 
